3cdd452bdba07d4ca6f9bfbffc94b8ab20099ba4
ShibabOfficial <88441414+ShibabOfficial@users.noreply.github.com>
2023-12-11 23:24:39 +0100

    Add files via upload

---
+++ cwb/js/main.js
@@ -0,0 +1,23 @@
+const languages = [
+    {
+        name: "c++",
+        highlights: [
+
+        ],
+    }
+];
+
+function highlightAll() {
+    document.querySelectorAll("code .code").forEach((cd) => {
+        let text = cd.innerText;
+        languages.forEach((lang) => {
+            if (lang.name == cd.getAttribute("lang")) {
+                text.split("\n").forEach((line) => {
+                    line.split(/ +/).forEach((word) => {
+                        console.log(word);
+                    });
+                });
+            }
+        });
+    });
+}
