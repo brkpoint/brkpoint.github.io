4c927d76de8818a080b7a1b995bb20edafe90170
ShibabOfficial <88441414+ShibabOfficial@users.noreply.github.com>
2024-02-21 23:20:40 +0100

    Major visual update for the website and new things
    added:
    - projects (not all)
    - remade the UI
    - repaired somethings

--- cwb/js/main.js
+++ cwb/js/main.js
@@ -2,22 +2,25 @@ const languages = [
     {
         name: "c++",
         highlights: [
-            { word: "", color: [] }
+            { word: "", color: [0, 0, 0] }
         ],
     }
 ];
 
 function highlightAll() {
-    document.querySelectorAll("code .code").forEach((cd) => {
-        let text = cd.innerText;
+    document.querySelectorAll("code").forEach((cd) => {
+        const cf = cd.querySelector(".code"), cl = cd.getAttribute("lang");
+        let text = cf.innerText, ttext;
         languages.forEach((lang) => {
-            if (lang.name == cd.getAttribute("lang")) {
-                text.split("\n").forEach((line) => {
-                    line.split(/ +/).forEach((word) => {
-                        console.log(word);
-                    });
+            console.log(lang)
+            if (lang.name !== cl)
+                return;
+
+            text.split("\n").forEach((line) => {
+                line.split(/ +/).forEach((word) => {
+                    
                 });
-            }
+            });
         });
     });
 }
