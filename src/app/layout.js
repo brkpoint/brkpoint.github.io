d1fd2ea52d013fb7cb4b194b401ab8699db4ad62
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-17 22:12:08 +0200

    idk what it did

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import 'simplebar-react/dist/simplebar.min.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 import useSticky from "@/modules/Sticky";
 
