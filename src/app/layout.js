94821fcfffca89309c3cfd0de08515e8ebc69c58
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-17 16:00:54 +0200

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "brkpoint",
-  description: "Personal page",
+  title: "Aleksander",
+  description: "Welcome to my page!!",
+  creator: "Aleksander",
+  og: [{ image: "https://avatars.githubusercontent.com/u/88441414" }]
 };
 
 export default function RootLayout({ children }) {
