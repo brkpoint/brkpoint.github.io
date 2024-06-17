030bf03bfbce6a13a5d60b1f282a54648d1182b6
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-17 20:50:46 +0200

    fixeszzz

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full overflow-auto">
       <head>
         <link rel="icon" href="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/88441414&mask=circle" sizes="any"/>
       </head>
