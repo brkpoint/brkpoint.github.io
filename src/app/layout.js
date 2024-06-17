5d402e807fb9fc55b424fa9724749d47755168dc
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-17 19:30:38 +0200

    fixz

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,6 @@ export const metadata = {
   title: "Aleksander",
   description: "Welcome to my page!!",
   creator: "Aleksander",
-  image: "https://avatars.githubusercontent.com/u/88441414",
   openGraph: {
     image: "https://avatars.githubusercontent.com/u/88441414?s=48&v=4"
   },
@@ -16,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <link rel="icon" href="https://avatars.githubusercontent.com/u/88441414" sizes="any"/>
+      </head>
       <body className={`${inter.className} flex flex-col font-mono h-full`}>
         {children}
       </body>
