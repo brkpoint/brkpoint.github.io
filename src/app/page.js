23989e564132a60a9907bd470d7e5ba40f2cfe72
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-18 19:30:58 +0200

    some small fixes... yeah...

--- src/app/page.js
+++ src/app/page.js
@@ -9,56 +9,63 @@ export default function Home() {
   const { sticky, stickyRef } = useSticky();
   return (
     <>
-      <nav ref={stickyRef} className={`z-10 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
-        <div className="border border-zinc-400 shadow-lg rounded m-5 p-2 flex align-middle justify-between w-full">
-          <div className="flex flex-row items-center">
-            <Image
-              className="rounded-full"
-              src="https://avatars.githubusercontent.com/u/88441414"
-              alt="pfp"
-              width={32}
-              height={32}
-            />
-            <span className="text-lg ml-2">Aleksander</span>
-          </div>
-          <div className="flex flex-row text-xl items-center">
-            /home
-          </div>
-          <ul className="flex flex-row justify-around items-center">
-            <li className="mr-2">
-              <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
-            </li>
-            <li className="mr-2">
-              <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
-            </li>
-            <li className="mr-3">
-              <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
-            </li>
-            <li>
-              <Link href="secret">
+      <main className="flex flex-col items-center">
+        <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
+          <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
+            <div className="flex w-full justify-between" ref={stickyRef}>
+              <div className="flex flex-row items-center">
                 <Image
-                  src="/cat2.gif"
-                  alt="kitty"
-                  width={24}
-                  height={24}
+                  className="rounded-full"
+                  src="https://avatars.githubusercontent.com/u/88441414"
+                  alt="pfp"
+                  width={32}
+                  height={32}
                 />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <main className="m-4 flex min-h-screen flex-col items-center">
-        <h1 className="text-4xl">
-          Hello!
-        </h1>
-        <p className="mt-1">
-          This is my website, its still under developement...
-        </p>
-        <div className="w-full">
-
+                <span className="text-lg ml-2">Aleksander</span>
+              </div>
+              <div className="flex flex-row text-xl items-center">
+                /home
+              </div>
+              <ul className="flex flex-row justify-around items-center">
+                <li className="mr-2">
+                  <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
+                </li>
+                <li className="mr-2">
+                  <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
+                </li>
+                <li className="mr-3">
+                  <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
+                </li>
+                <li>
+                  <Link href="secret">
+                    <Image
+                      src="/cat2.gif"
+                      alt="kitty"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        
+        <div className="content p-2 flex flex-col items-center w-full">
+          <h1 className="text-4xl">
+            Hello!
+          </h1>
+          <p className="mt-1">
+            This is my website, its still under developement...
+          </p>
+          <div>
+          
+          </div>
         </div>
       </main>
+      <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
+        <p>©2024 brkpoint | All Rights Reserved</p>
+      </footer>
     </>
   );
 }
--- src/app/secret/page.js
+++ src/app/secret/page.js
@@ -9,44 +9,50 @@ export default function Home() {
     const { sticky, stickyRef } = useSticky();
     return (
         <>
-            <nav ref={stickyRef} className={`z-10 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
-                <div className="border border-zinc-400 shadow-lg rounded m-5 p-2 flex align-middle justify-between w-full">
-                    <div className="flex flex-row items-center">
-                        <Image
-                            className="rounded-full"
-                            src="https://avatars.githubusercontent.com/u/88441414"
-                            alt="pfp"
-                            width={32}
-                            height={32}
-                        />
-                        <span className="text-lg ml-2">Aleksander</span>
+            <main className="flex flex-col h-full items-center">
+                <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
+                    <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
+                        <div className="flex w-full justify-between" ref={stickyRef}>
+                            <div className="flex flex-row items-center">
+                                <Image
+                                    className="rounded-full"
+                                    src="https://avatars.githubusercontent.com/u/88441414"
+                                    alt="pfp"
+                                    width={32}
+                                    height={32}
+                                />
+                                <span className="text-lg ml-2">Aleksander</span>
+                            </div>
+                            <div className="flex flex-row text-xl items-center">
+                                /secret
+                            </div>
+                            <ul className="flex flex-row justify-around items-center">
+                                <li className="mr-2">
+                                    <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
+                                </li>
+                                <li className="mr-2">
+                                    <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
+                                </li>
+                                <li>
+                                    <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="flex flex-row text-xl items-center">
-                        /secret
-                    </div>
-                    <ul className="flex flex-row justify-around items-center">
-                        <li className="mr-2">
-                            <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
-                        </li>
-                        <li className="mr-2">
-                            <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
-                        </li>
-                        <li>
-                            <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+                </nav>
 
-            <main className="m-4 flex h-full flex-col items-center">
-                <Image
-                    className="self-center"
-                    src="/cat2.gif"
-                    alt="kitty"
-                    width={128}
-                    height={128}
-                />
+                <div className="content p-2 flex flex-col items-center justify-center h-full w-full">
+                    <Image
+                        src="/cat2.gif"
+                        alt="kitty"
+                        width={128}
+                        height={128}
+                    />
+                </div>
             </main>
+            <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
+                <p>©2024 brkpoint | All Rights Reserved</p>
+            </footer>
         </>
     );
 }
