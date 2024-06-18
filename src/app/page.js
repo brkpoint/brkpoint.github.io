b3c24eb1827947c37123bde699c2f069a59af0b2
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-19 00:27:38 +0200

    i hate bad written docs (overlayscrollbars-react)...

--- src/app/page.js
+++ src/app/page.js
@@ -5,143 +5,124 @@ import { useEffect, useState, useRef } from "react"
 import Image from "next/image";
 import Link from "next/link";
 
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import { OverlayScrollbarsComponent, useOverlayScrollbars } from "overlayscrollbars-react";
 
 export default function Home() {
-  const osRef = useRef();
-  const { current } = osRef;
-
   const stickyRef = useRef(null);
   const [sticky, setSticky] = useState(false);
   const [offset, setOffset] = useState(0);
 
+  const osRef = useRef(null);
+
+  const [osApplied, setosApplied] = useState(null);
+  const [initialize, instance] = useOverlayScrollbars({
+    defer: true,
+    events: {
+      initialized: () => {
+        setosApplied(true);
+      },
+      destroyed: () => {
+        setosApplied(false);
+      },
+    },
+  });
+  
+  useEffect(() => {
+    initialize(document.body);
+  }, [initialize]);
+
   return (
-    <OverlayScrollbarsComponent
-      ref={osRef}
-      className="h-full"
-      options={{ scrollbars: { autoHide: 'scroll' } }}
-      events={{ scroll: () => {
-        const osInstance = current?.osInstance();
-
-        const { scrollOffsetElement } = osInstance.elements();
-        const { scrollLeft, scrollTop } = scrollOffsetElement;
-
-        setSticky(scrollTop > stickyRef.current.offsetTop);
-      } }}
-      defer
-    >
-      <main className="flex flex-col min-h-screen items-center">
-        <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
-          <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
-            <div className="flex w-full justify-between" ref={stickyRef}>
-              <div className="flex flex-row items-center">
-                <Image
-                  className="rounded-full"
-                  src="https://avatars.githubusercontent.com/u/88441414"
-                  alt="pfp"
-                  width={32}
-                  height={32}
-                />
-                <span className="text-lg ml-2">Aleksander</span>
+    <>
+    {osApplied ? (
+      <OverlayScrollbarsComponent
+        ref={osRef}
+        className="h-full"
+        options={{ scrollbars: { autoHide: 'scroll' } }}
+        events={{
+          initialized: () => {
+            setosApplied(true);
+          },
+          destroyed: () => {
+            setosApplied(false);
+          },
+          scroll: () => {
+            const { current } = osRef;
+            const osInstance = current?.osInstance();
+    
+            if (!osInstance) {
+              return;
+            }
+    
+            const { scrollOffsetElement } = osInstance.elements();
+            const { scrollLeft, scrollTop } = scrollOffsetElement;
+    
+            setSticky(scrollTop > stickyRef.current.offsetTop);
+          },
+        }}
+        defer
+      >
+        <main className="flex flex-col min-h-screen items-center">
+          <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
+            <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
+              <div className="flex w-full justify-between" ref={stickyRef}>
+                <div className="flex flex-row items-center">
+                  <Image
+                    className="rounded-full"
+                    src="https://avatars.githubusercontent.com/u/88441414"
+                    alt="pfp"
+                    width={32}
+                    height={32}
+                  />
+                  <span className="text-lg ml-2">Aleksander</span>
+                </div>
+                <div className="flex flex-row text-xl items-center">
+                  /home
+                </div>
+                <ul className="flex flex-row justify-around items-center">
+                  <li className="mr-2">
+                    <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
+                  </li>
+                  <li className="mr-2">
+                    <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
+                  </li>
+                  <li className="mr-3">
+                    <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
+                  </li>
+                  <li>
+                    <Link href="secret">
+                      <Image
+                        src="/cat2.gif"
+                        alt="kitty"
+                        width={24}
+                        height={24}
+                      />
+                    </Link>
+                  </li>
+                </ul>
               </div>
-              <div className="flex flex-row text-xl items-center">
-                /home
-              </div>
-              <ul className="flex flex-row justify-around items-center">
-                <li className="mr-2">
-                  <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
-                </li>
-                <li className="mr-2">
-                  <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
-                </li>
-                <li className="mr-3">
-                  <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
-                </li>
-                <li>
-                  <Link href="secret">
-                    <Image
-                      src="/cat2.gif"
-                      alt="kitty"
-                      width={24}
-                      height={24}
-                    />
-                  </Link>
-                </li>
-              </ul>
+            </div>
+          </nav>
+          
+          <div className="content p-2 flex flex-col items-center w-full">
+            <h1 className="text-4xl">
+              Hello!
+            </h1>
+            <p className="mt-1">
+              This is my website, its still under developement...
+            </p>
+            <div>
+
             </div>
           </div>
-        </nav>
-        
-        <div className="content p-2 flex flex-col items-center w-full">
-          <h1 className="text-4xl">
-            Hello!
-          </h1>
-          <p className="mt-1">
-            This is my website, its still under developement...
-          </p>
-          <div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-          </div>
-        </div>
-      </main>
+        </main>
 
-      <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
-        <p>© 2024 brkpoint | All Rights Reserved</p>
-      </footer>
-    </OverlayScrollbarsComponent>
+        <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
+          <p>© 2024 brkpoint | All Rights Reserved</p>
+        </footer>
+      </OverlayScrollbarsComponent>
+    ) : (
+      null
+    )}
+    </>
   );
 }
--- src/app/secret/page.js
+++ src/app/secret/page.js
@@ -5,77 +5,111 @@ import { useEffect, useState, useRef } from "react"
 import Image from "next/image";
 import Link from "next/link";
 
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import { OverlayScrollbarsComponent, useOverlayScrollbars } from "overlayscrollbars-react";
 
 export default function Home() {
-    const osRef = useRef();
-    const { current } = osRef;
-
     const stickyRef = useRef(null);
     const [sticky, setSticky] = useState(false);
     const [offset, setOffset] = useState(0);
 
+    const osRef = useRef(null);
+
+    const [osApplied, setosApplied] = useState(null);
+    const [initialize, instance] = useOverlayScrollbars({
+        defer: true,
+        events: {
+            initialized: () => {
+                setosApplied(true);
+            },
+            destroyed: () => {
+                setosApplied(false);
+            },
+        },
+    });
+
+    useEffect(() => {
+        initialize(document.body);
+    }, [initialize]);
+
     return (
-        <OverlayScrollbarsComponent
-            ref={osRef}
-            className="h-full"
-            options={{ scrollbars: { autoHide: 'scroll' } }}
-            events={{
-                scroll: () => {
-                    const osInstance = current?.osInstance();
+        <>
+            {osApplied ? (
+                <OverlayScrollbarsComponent
+                    ref={osRef}
+                    className="h-full"
+                    options={{ scrollbars: { autoHide: 'scroll' } }}
+                    events={{
+                        initialized: () => {
+                            setosApplied(true);
+                        },
+                        destroyed: () => {
+                            setosApplied(false);
+                        },
+                        scroll: () => {
+                            const { current } = osRef;
+                            const osInstance = current?.osInstance();
 
-                    const { scrollOffsetElement } = osInstance.elements();
-                    const { scrollLeft, scrollTop } = scrollOffsetElement;
+                            if (!osInstance) {
+                                return;
+                            }
 
-                    setSticky(scrollTop > stickyRef.current.offsetTop);
-                }
-            }}
-            defer
-        >
-            <main className="flex flex-col min-h-screen items-center">
-                <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
-                    <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
-                        <div className="flex w-full justify-between" ref={stickyRef}>
-                            <div className="flex flex-row items-center">
-                                <Image
-                                    className="rounded-full"
-                                    src="https://avatars.githubusercontent.com/u/88441414"
-                                    alt="pfp"
-                                    width={32}
-                                    height={32}
-                                />
-                                <span className="text-lg ml-2">Aleksander</span>
+                            const { scrollOffsetElement } = osInstance.elements();
+                            const { scrollLeft, scrollTop } = scrollOffsetElement;
+
+                            setSticky(scrollTop > stickyRef.current.offsetTop);
+                        },
+                    }}
+                    defer
+                >
+                    <main className="flex flex-col min-h-screen items-center">
+                        <nav className={`z-10 p-3 flex-row text-sm w-full flex${sticky ? " sticky" : ""}`}>
+                            <div className={`p-3 flex rounded align-middle navbar-border w-full${sticky ? " shadow-2xl" : ""}`}>
+                                <div className="flex w-full justify-between" ref={stickyRef}>
+                                    <div className="flex flex-row items-center">
+                                        <Image
+                                            className="rounded-full"
+                                            src="https://avatars.githubusercontent.com/u/88441414"
+                                            alt="pfp"
+                                            width={32}
+                                            height={32}
+                                        />
+                                        <span className="text-lg ml-2">Aleksander</span>
+                                    </div>
+                                    <div className="flex flex-row text-xl items-center">
+                                        /home
+                                    </div>
+                                    <ul className="flex flex-row justify-around items-center">
+                                        <li className="mr-2">
+                                            <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
+                                        </li>
+                                        <li className="mr-2">
+                                            <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div className="flex flex-row text-xl items-center">
-                                /secret
-                            </div>
-                            <ul className="flex flex-row justify-around items-center">
-                                <li className="mr-2">
-                                    <Link href="/" className="hover:font-bold hover:-translate-y-2">/home</Link>
-                                </li>
-                                <li className="mr-2">
-                                    <Link href="/projects" className="hover:font-bold hover:-translate-y-2">/projects</Link>
-                                </li>
-                                <li>
-                                    <Link href="https://github.com/brkpoint" className="hover:font-bold hover:-translate-y-2">/about</Link>
-                                </li>
-                            </ul>
+                        </nav>
+
+                        <div className="content p-2 flex flex-col items-center justify-center min-h-screen w-full">
+                            <Image
+                                src="/cat2.gif"
+                                alt="kitty"
+                                width={128}
+                                height={128}
+                            />
                         </div>
-                    </div>
-                </nav>
+                    </main>
 
-                <div className="content p-2 flex flex-col items-center justify-center h-full w-full">
-                    <Image
-                        src="/cat2.gif"
-                        alt="kitty"
-                        width={128}
-                        height={128}
-                    />
-                </div>
-            </main>
-            <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
-                <p>© 2024 brkpoint | All Rights Reserved</p>
-            </footer>
-        </OverlayScrollbarsComponent>
+                    <footer className="p-4 footer flex flex-col items-center justify-center text-xs">
+                        <p>© 2024 brkpoint | All Rights Reserved</p>
+                    </footer>
+                </OverlayScrollbarsComponent>
+            ) : (
+                null
+            )}
+        </>
     );
 }
